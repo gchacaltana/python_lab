f1c2e613b6cf7ecc7c34ab39576a368600a62ef9
Gonzalo Chacaltana <gchacaltanab@gmail.com>
2019-09-19 22:15:17 -0500

    add natural numbers

---
+++ Lab01/lab01_ejercicio01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Programa que imprima los 25 números naturales
+"""
+__author__ = "Gonzalo Chacaltana Buleje"
+__email__ = "gchacaltanab@gmail.com"
+
+if __name__ == "__main__":
+    n_start = 1; n_end = 25
+    print([n for n in range(n_start,(n_end + 1))])
